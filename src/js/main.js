e9ffda0c3607693b993b58f2e3583a67ebe99c88
jefferson cabral silva <jemec27@gmail.com>
2023-07-15 01:47:23 -0300

    kickbank

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   new TypeIt(".animated", {
-    speed: 500,
+    speed: 200,
     waitUntilVisible: true,
     loop: true,
-  }).type("É prático!").delete(10).type("É rápido!").delete(10).type("É seguro").go();
+  }).type("É prático!  ").delete(14).type("É rápido!  ").delete(14).type("É seguro!  ").go();
 })
 
 
